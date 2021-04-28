beb3303b9f16b7111b76355d957b2331af67cd58
Mschulen <mateusz.schulen@eengine.pl>
2021-04-28 16:48:01 +0200

    got the derive or decive or whatever shit to work, kinda tired of it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  devise_scope :user do #bullshit that makes devise work
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
+
+  root "welcome#index"
 end
