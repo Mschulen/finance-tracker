49506cff3a1036848748920bc198ffb1834c52b5
Mschulen <mateusz.schulen@eengine.pl>
2021-04-29 16:38:34 +0200

    added uhh idk

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,13 +4,17 @@
 // that code so it'll be compiled.
 //= require jquery
 //= require jquery_ujs
-//= require javascript/dropdown
+//= require turbolinks
+//= require bootstrap
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
+// import "bootstrap"
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
+
 
 
 Rails.start()
